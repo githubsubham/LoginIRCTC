4be1f628f676880238bd7af8e9a4a900137b8fc7
subham@0693 <papu.ajay17@gmail.com>
2023-01-06 11:19:38 +0530

    second commit

--- LoginRailway/src/ReverseString.java
+++ LoginRailway/src/ReverseString.java
@@ -2,7 +2,13 @@
 public class ReverseString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str="Java Is Very Easy";
+		String temp="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			temp=temp+str.charAt(i);
+		}
+		System.out.println(temp);
 
 	}
 
